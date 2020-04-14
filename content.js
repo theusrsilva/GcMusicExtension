f593e99819692dbeac8ce0723074f30cae73f09e
Matheus Rocha desktop <theusrsilva@gmail.com>
2020-04-14 19:23:30 -0300

    done

---
+++ content.js
@@ -0,0 +1,13 @@
+
+const interval = setInterval(()=>{
+    const audio = document.getElementById("soundHandle1");
+    if(audio){
+        const ss = audio.firstElementChild;
+        console.log(ss);
+        const url = chrome.runtime.getURL('musica.mp3');
+        ss.src = url;
+        audio.load();
+        audio.play();
+        clearInterval(interval);
+    }
+})
