5463bfdc92779ca5e5d9040720220e068f1c5b84
Matheus Rocha desktop <theusrsilva@gmail.com>
2020-04-14 19:45:53 -0300

    fazendo ajustes

--- content.js
+++ content.js
@@ -1,10 +1,9 @@
 
-const interval = setInterval(()=>{
-    const audio = document.getElementById("soundHandle1");
-    if(audio){
-        const ss = audio.firstElementChild;
+const interval    = setInterval(()=>{
+    const audio   = document.getElementById("soundHandle1");
+    if(audio.src="/sounds/lobby-beta/pre-ready.mp3"){
         const url = chrome.runtime.getURL('musica.mp3');
-        ss.src = url;
+        audio.src = url;
         audio.load();
         audio.play();
         clearInterval(interval);
