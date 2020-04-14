7f86ae9502261a026c59ee5bd6a28faf586dd3b2
Matheus Rocha desktop <theusrsilva@gmail.com>
2020-04-14 20:13:52 -0300

    não esta funcionando

--- content.js
+++ content.js
@@ -1,11 +1,13 @@
 
 const interval    = setInterval(()=>{
     const audio   = document.getElementById("soundHandle1");
-    if(audio.src="/sounds/lobby-beta/pre-ready.mp3"){
-        const url = chrome.runtime.getURL('musica.mp3');
-        audio.src = url;
-        audio.load();
-        audio.play();
-        clearInterval(interval);
+    if(audio){
+        if(audio.src="/sounds/lobby-beta/pre-ready.mp3"){
+            const url = chrome.runtime.getURL('musica.mp3');
+            console.log(url);
+            audio.src = url;
+            audio.load();
+            audio.play();
+        }
     }
 })
